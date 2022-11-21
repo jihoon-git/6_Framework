3abf077bc6f359d02829f46f1c574074dce84fe9
jihoon-git <hooon.h@gmail.com>
2022-11-21 09:09:36 +0900

    11/18 게시글 상세조회

--- project2/src/main/java/edu/kh/project/board/controller/BoardController.java
+++ project2/src/main/java/edu/kh/project/board/controller/BoardController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.kh.project.board.model.service.BoardService;
+import edu.kh.project.board.model.vo.Board;
 
 @Controller
 public class BoardController {
@@ -32,6 +33,7 @@ public class BoardController {
 	
 	@GetMapping("/board/{boardCode}")
 	public String selectBoardList(@PathVariable("boardCode") int boardCode,
+			//@PathVariable("boardCode") 주소에 있는 값을 변수로 사용할 수 있게 해줌
 			Model model,
 			@RequestParam(value="cp", required=false, defaultValue = "1") int cp
 			) {
@@ -45,5 +47,21 @@ public class BoardController {
 		model.addAttribute("map", map); // request scope 세팅 
 		
 		return "board/boardList"; // forword
+	}
+	
+	@GetMapping("/board/{boardCode}/{boardNo}")
+	public String boardDetail(
+			@PathVariable("boardNo") int boardNo,
+			@PathVariable("boardCode") int boardCode,
+			Model model) {
+		
+		// 게시글 상세조회 서비스 호출
+		Board board = service.selectBoardDetail(boardNo);
+		// + 좋아요 수, 좋아요 여부
+		// + 조회 수 증가(쿠키를 이용해서 해당 IP당 하루 한번)
+		
+		model.addAttribute("board", board);
+		
+		return "board/boardDetail";
 	}
 }
